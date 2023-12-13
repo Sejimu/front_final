6eba96c0024f2f5b67b5d00e81df61b750196a69
Sejimu <snursultan322@gmail.com>
2023-12-13 20:37:25 +0600

    deploy

--- src/component/TodoList.tsx
+++ src/component/TodoList.tsx
@@ -2,7 +2,6 @@ import { useEffect } from "react";
 import { useTodoContext } from "../contexts/TodoContext";
 import ListGroup from "react-bootstrap/ListGroup";
 import TodoItem from "./TodoItem";
-import Button from "react-bootstrap/Button";
 import RandomTaskGenerator from "./RandomTaskGenerator";
 
 function TodoList(): JSX.Element {
